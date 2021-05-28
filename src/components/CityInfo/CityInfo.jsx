6461adf17b77cb59f9ea38ecef51e9845a8546cb
Rodrigo Hernandez <rodrigo.hernandez@warnermedia.com>
2021-05-28 00:10:25 -0300

    add material UI Typography component

--- src/components/CityInfo/CityInfo.jsx
+++ src/components/CityInfo/CityInfo.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Typography from '@material-ui/core/Typography'
 
 const CityInfo = ({city, country}) => {
     return (
         <>
-            
+            <Typography>{city}</Typography>
+            <Typography>{country}</Typography>
+
         </>
     )
 }
