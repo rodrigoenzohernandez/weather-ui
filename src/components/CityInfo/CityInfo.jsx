09aefe13c16423f234c8a26d77a98c0435e53ce1
Rodrigo Hernandez <rodrigo.hernandez@warnermedia.com>
2021-05-27 23:57:10 -0300

    create component CityInfo

---
+++ src/components/CityInfo/CityInfo.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const CityInfo = ({city, country}) => {
+    return (
+        <>
+            
+        </>
+    )
+}
+
+CityInfo.propTypes = {
+    city: PropTypes.string.isRequired,
+    country: PropTypes.string.isRequired
+}
+
+export default CityInfo
